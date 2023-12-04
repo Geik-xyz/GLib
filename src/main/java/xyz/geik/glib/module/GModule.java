1465a6ec922f25699924b11a687a910f927b7b4e
Poyraz INAN <ben@poyrazinan.com.tr>
2023-12-04 19:06:33 +0300

    Update GModule.java

--- src/main/java/xyz/geik/glib/module/GModule.java
+++ src/main/java/xyz/geik/glib/module/GModule.java
@@ -1,9 +1,14 @@
 package xyz.geik.glib.module;
 
 
+import eu.okaeri.configs.ConfigManager;
+import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.yaml.bukkit.YamlBukkitConfigurer;
 import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +22,30 @@ import java.util.stream.IntStream;
  * @since 1.0
  */
 public abstract class GModule implements Modulable {
+
+    @Getter
+    private OkaeriConfig configFile;
+
+    /**
+     * Registers config to file
+     * @param configClass config class
+     * @param instance of plugin
+     */
+    public void registerConfig(Class<OkaeriConfig> configClass, JavaPlugin instance) {
+        this.configFile = ConfigManager.create(configClass, (it) -> {
+            it.withConfigurer(new YamlBukkitConfigurer());
+            it.withBindFile(new File(instance.getDataFolder() + "/modules/" + getName(), "config.yml"));
+            it.saveDefaults();
+            it.load(true);
+        });
+    }
+
+    /**
+     * Reloads config file
+     */
+    public void reloadConfig() {
+        getConfigFile().load(true);
+    }
 
     /**
      * List of dependencies contains dependency tree
